da4b08c76fca8973afa0707c1ab56b07c0b898d5
@helloncanella <helloncanella@gmail.com>
2015-09-18 22:55:13 -0300

    do tests of popcorn time

--- main/public/js/uploader.js
+++ main/public/js/uploader.js
@@ -3,7 +3,41 @@
   var acceptedSubtitles = ["text/vtt", "application/x-subrip"];
   var supportedVideos = ["video/mp4", "video/ogg", "video/webm"];
 
-  var videoFile, subtitleFile, subtitleURL;
+  var videoFile, subtitleFile, subtitleURL, videoURL;
+
+  //verify if video and subtitle is loaded
+  function verifier() {
+
+    if (subtitleURL && videoURL) {
+      track.src = subtitleURL;
+
+      $('#cinema').prepend(video);
+      $('video').append(track);
+      $('.uploader').remove();
+      $('#cinema').addClass("show-block");
+
+      video = $('video')[0]
+
+      video.play();
+
+      textTrack = video.textTracks[0];
+      textTrack.mode = "hidden";
+      textTrackList = textTrack.cues;
+
+      textTrack.oncuechange = function() {
+        var currentSubtitle;
+        currentCue = this.activeCues[0];
+        $(".controls").css("visibility", "hidden");
+        $('#subtitles h1').text("");
+        if (currentCue) {
+          currentSubtitle = currentCue.text;
+          $(".controls").css("visibility", "visible");
+          $('#subtitles h1').text(currentSubtitle);
+        }
+      };
+    }
+
+  }
 
   $("#dropRegion").on({
     dragenter: function(event) {
@@ -34,12 +68,22 @@
         } else {
           var r = confirm("Replace the added movie?");
           if (r == true) {
-            videofile = file;
+            videoFile = file;
           }
         }
 
+        video.src = videoURL = window.URL.createObjectURL(videoFile);
+
+        verifier();
+
       } else if (isSubtitle) {
+
         if (!subtitleFile) {
+
+          if (!videoFile) {
+            $("#dropRegion h1").text("Add the video now")
+          }
+
           subtitleFile = file;
           if (format == "text/vtt") {
             subtitleURL = window.URL.createObjectURL(subtitleFile);
@@ -51,16 +95,14 @@
                 fileName: file.name
               }, function(vttURL) {
                 subtitleURL = vttURL;
-                console.log(vttURL);
-              }).fail(function(){
-                console.log("error");
+                verifier();
+              }).fail(function(error) {
+                alert(error);
               })
             }
             reader.readAsBinaryString(file);
           }
-          if (!videoFile) {
-            $("#dropRegion h1").text("Add the video now")
-          }
+
         } else {
           var r = confirm("Replace the added subtitle?");
           if (r == true) {
@@ -71,47 +113,8 @@
         alert(format + " isn't supported")
       }
 
-      if (videoFile && subtitleFile) {
-
-        video.src = window.URL.createObjectURL(videoFile);
-        track.src = subtitleURL;
-
-        $('#cinema').prepend(video);
-        $('video').append(track);
-        $('.uploader').remove();
-        $('#cinema').addClass("show-block");
-
-        $('video').trigger('oncanplay');
-
-        video.oncanplay = function(){
-
-          textTrack = video.textTracks[0];
-          console.log(textTrack);
-          textTrack.mode="hidden";
-          textTrackList = textTrack.cues;
-
-          this.play();
-
-          textTrack.oncuechange = function() {
-            var currentSubtitle;
-            currentCue = this.activeCues[0];
-            $(".controls").css("visibility", "hidden");
-            $('#subtitles h1').text("");
-            if (currentCue) {
-              currentSubtitle = currentCue.text;
-              $(".controls").css("visibility", "visible");
-              return $('#subtitles h1').text(currentSubtitle);
-            }
-          };
-        }
-
-      }
     }
-  })
+
+  });
+
 })(jQuery, window, document, video, textTrack, track)
-
-
-
-
-// blob:http%3A//localhost%3A3000/a379b2cf-8684-4776-86b2-c6fb2391820a blob:http%3A//localhost%3A3000/c3c78ad4-cdf2-4f5d-9c00-7188c13b9dd3
-//
